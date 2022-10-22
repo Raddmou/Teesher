9037c199a96a04561315718ed3ecfc282ba4e442
MouradM <wassakh@gmail.com>
2022-10-22 20:14:19 +0200

    -feat: front structure - feat: teeshcollection SC

---
+++ contracts/teeshcollection.sol
@@ -0,0 +1,44 @@
+//SPDX-License-Identifier: MIT
+pragma solidity solidity ^0.8.9;
+
+
+//import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+//learn more: https://docs.openzeppelin.com/contracts/3.x/erc721
+
+// GET LISTED ON OPENSEA: https://testnets.opensea.io/get-listed/step-two
+
+contract TeeshCollection is ERC1155, Ownable {
+  uint64 public cost = 0.10 ether;
+  uint256 public tokenSupply;
+  uint256 public maxSupply = 100;
+
+  constructor(string memory _uri) ERC1155(_uri) 
+  {}
+
+  function mint(
+      address _to,
+		  uint256 _quantity,
+		  bytes memory _data
+  )
+      public
+      onlyOwner
+  {
+        require(tokenSupply + _quantity < maxSupply - 1, "Max supply error");
+
+        _mint(_to, 0, _quantity, _data);
+        tokenSupply = tokenSupply + _quantity;
+      _incrementTokenTypeId();
+  }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 amount,
+        bytes memory data
+    ) public virtual override zebrasAreNotLocked(id) {
+        super.safeTransferFrom(from, to, 0, amount, data);
+    }
+}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import {ethers} from 'ethers';
+import Lock from './artifacts/contracts/Lock.sol/Lock.json';
 import './App.css';
+import imgUrl from './img/d1a2d30d74c79ea5ad5c62ea746f148f.jpg'
+
+const SmartContractAddress = "0x"
 
 function App() {
+
+  const [error, setError] = useState('');
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  async function fetchData(){
+    if(typeof window.ethereum !== 'undefined'){
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(SmartContractAddress, Lock.abi, provider);
+      try {
+        const cost = await contract.cost();
+        const totalSupply = await contract.totalSupply();
+        const object = {"cost": String(cost), "totalSupply" : String(totalSupply)};
+        setData(object);
+      } catch (error) {
+        setError(error.message);
+      }
+    }
+  }
+
+  async function mint() {
+    if(typeof window.ethereum !== 'undefined'){
+      let accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(SmartContractAddress, Lock.abi, signer);
+      try {
+        let ovverides = {
+          from: accounts[0],
+          value: data.cost
+        }
+        const transaction = await contract.mint(accounts[0], 1, ovverides);
+        await transaction.wait();
+        fetchData();
+      } catch (error) {
+        setError(error.message);
+      }
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <div className='banniere'>
+          <img src={imgUrl}/>
+        </div>
+        {error && <p>{error}</p>}
+        <h1>h1</h1>
+        <p className='count'>{data.totalSupply} / 1000</p>
+        <p className='cost'>Price {data.cost / 10**18}</p>
+        <button onClick={mint}>Mint</button>
+      </div>
     </div>
   );
 }
